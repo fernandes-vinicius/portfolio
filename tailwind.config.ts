1199413035a44a7aa9e8877d231c7ff2f48888b5
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2024-05-28 20:06:43 -0300

    chore: add new project v2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,20 @@
-import type { Config } from 'tailwindcss'
-import { fontFamily } from 'tailwindcss/defaultTheme'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: ['class'],
-  content: ['./src/**/*.tsx'],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    screens: {
-      '2xl': { max: '1535px' },
-      xl: { max: '1279px' },
-      lg: { max: '1023px' },
-      md: { max: '767px' },
-      sm: { max: '639px' },
-      xs: { max: '479px' },
-    },
     extend: {
-      fontFamily: {
-        sans: ['var(--font-sans)', ...fontFamily.sans],
-      },
-      colors: {
-        primary: 'hsl(var(--primary))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-
-export default config
+};
+export default config;
