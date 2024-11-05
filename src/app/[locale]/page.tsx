1935e9b685625f981ee188bf6ec0912e5d3e88b2
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2024-11-05 13:55:34 -0300

    feat: add intl

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,14 @@
+import { useTranslations } from "next-intl";
+
+import { Link } from "@/i18n/routing";
+
+export default function HomePage() {
+  const t = useTranslations("HomePage");
+
+  return (
+    <div>
+      <h1>{t("title")}</h1>
+      <Link href="/about">{t("about")}</Link>
+    </div>
+  );
+}
