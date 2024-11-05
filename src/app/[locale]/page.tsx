83e309ac0a2fdd83f6847ccce285d0d18b27464d
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2024-11-05 14:01:33 -0300

    chore: shadcn-ui setup

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,12 +3,12 @@ import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/routing";
 
 export default function HomePage() {
-  const t = useTranslations("HomePage");
+	const t = useTranslations("HomePage");
 
-  return (
-    <div>
-      <h1>{t("title")}</h1>
-      <Link href="/about">{t("about")}</Link>
-    </div>
-  );
+	return (
+		<div>
+			<h1>{t("title")}</h1>
+			<Link href="/about">{t("about")}</Link>
+		</div>
+	);
 }
