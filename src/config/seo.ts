774b9ff0554325de51978ca6c110afed24145dae
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2024-11-05 14:20:01 -0300

    chore: add SEO configs

---
+++ src/config/seo.ts
@@ -0,0 +1,18 @@
+export const SEO = {
+	title: "Vinicius Portfolio",
+	description: "Modern & Minimal Portfolio",
+	openGraph: {
+		title: "Vinicius Portfolio",
+		description: "Vinicius Portfolio",
+		url: "https://vinicius-portfolio.vercel.app",
+		siteName: "Vinicius Portfolio",
+		images: [
+			{
+				url: "https://vinicius-portfolio.vercel.app/api/og",
+				width: 1200,
+				height: 630,
+			},
+		],
+	},
+	// ...
+};
