a20c12c1d629ca714f7829bc5dd8680fb2fb8375
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2024-11-05 14:16:04 -0300

    chore: fonts setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,6 +56,9 @@ const config: Config = {
 				md: "calc(var(--radius) - 2px)",
 				sm: "calc(var(--radius) - 4px)",
 			},
+			fontFamily: {
+				sans: ["var(--font-sans)"],
+			},
 		},
 	},
 	plugins: [require("tailwindcss-animate")],
